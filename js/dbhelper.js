630877564cccf6218c7f6a3b07ef751c118bc47b
Joe O'Connor <joconnorje@gmail.com>
2018-06-24 21:53:39 -0400

    Change CSS to make responsive,set media queries

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    /*const port = 8000; // Change this to your server port */
+    return "http://127.0.0.1:8000/data/restaurants.json"; //was set to localhost and in single quotes
   }
 
   /**
