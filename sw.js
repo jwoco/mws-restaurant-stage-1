a63027e4eacf7d9114214ecb4009c929075e2886
Joe O'Connor <joconnorje@gmail.com>
2018-06-30 21:04:46 -0400

    added code to create cache and install files in cache

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
 
 /* This file contains the functions used by the serviceworker to display app when offline.   */
-self.addEventListener('fetch', function (event) {
-	console.log(event.request);
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('mws-restaurants-v1').then(function(cache) {
+			return cache.addAll([
+				'/',
+				'index.html',
+				'restaurant.html',
+				'/css/styles.css',
+				'js/main.js',
+				'js/dbhelper.js',
+				'js/restaurant_info.js'
+				]);
+		})
+	);
 });
+
+ /* self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		new Response('hell world')
+		);
+}); */
